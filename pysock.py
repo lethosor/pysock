0ef77280d04468847763a09d8503fe9fd0ab30f5
lethosor <lethosor@gmail.com>
2014-10-05 14:34:25 -0400

    Initial Client implementation

--- pysock.py
+++ pysock.py
@@ -37,29 +37,46 @@ class Protocol:
 class EventHandler:
     def __init__(self):
         self.callbacks = {}
+        self.handlers = []
         self.lock = threading.RLock()
 
-    def bind(self, event, handler):
+    def bind(self, event, callback):
         with self.lock:
             if not event in self.callbacks:
                 self.callbacks[event] = []
-            if handler not in self.callbacks[event]:
-                self.callbacks[event].append(handler)
+            if callback not in self.callbacks[event]:
+                self.callbacks[event].append(callback)
 
-    def unbind(self, event, handler):
+    def add_handler(self, obj):
+        """ Bind all events to a corresponding 'on_event' method of obj, if available """
+        with self.lock:
+            self.handlers.append(obj)
+
+    def unbind(self, event, callback):
         with self.lock:
             if event not in self.callbacks:
                 raise ValueError('Unrecognized event: %s' % event)
-            if handler not in self.callbacks[event]:
+            if callback not in self.callbacks[event]:
                 raise ValueError('Handler not bound to event')
-            self.callbacks[event].remove(handler)
+            self.callbacks[event].remove(callback)
 
-    def trigger(self, event, *data):
+    def remove_handler(self, obj):
+        with self.lock:
+            if obj not in self.handlers:
+                raise ValueError('Handler not registered')
+            self.handlers.remove(obj)
+
+    def trigger(self, event, *args, **kwargs):
         with self.lock:
             if event not in self.callbacks:
                 self.callbacks[event] = []
-            for handler in self.callbacks[event]:
-                handler(*data)
+            for callback in self.callbacks[event]:
+                callback(*args, **kwargs)
+            for obj in self.handlers:
+                if hasattr(obj, 'on_' + event):
+                    callback = getattr(obj, 'on_' + event)
+                    if hasattr(callback, '__call__'):
+                        callback(*args, **kwargs)
 
 class SocketSendThread(threading.Thread):
     def __init__(self, socket, protocol):
@@ -118,17 +135,25 @@ class SocketRecvThread(threading.Thread):
 
 class SocketConnection:
     def __init__(self, socket, protocol):
+        self.connected = False
         self.socket = socket
         self.events = EventHandler()
         self.send_thread = SocketSendThread(socket, protocol)
         self.recv_thread = SocketRecvThread(socket, protocol)
+        self.recv_thread.events.bind('close', lambda: self.close())
 
     def listen(self):
         self.send_thread.start()
         self.recv_thread.start()
-        self.recv_thread.events.bind('close', lambda: self.close())
+        self.connected = True
+
+    def send(self, msg):
+        self.send_thread.send(msg)
 
     def close(self):
+        if not self.connected:
+            return
+        self.connected = False
         self.send_thread.send(SHUTDOWN)
         try:
             self.socket.shutdown(socket.SHUT_RDWR)
@@ -138,11 +163,58 @@ class SocketConnection:
                 pass
             else:
                 raise
+        finally:
+            self.events.trigger('close')
 
 class Server:
-    def __init__(self, host, port):
+    def __init__(self, host, port, protocol=None, reuse_addr=False):
         self.addr = (host, port)
+        self.protocol = protocol or self.protocol
+        if not self.protocol:
+            raise ValueError('Undefined protocol')
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if reuse_addr:
+            self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((host, port))
         self.socket.listen(4)
+
+class Client:
+    def __init__(self, host, port, protocol=None):
+        self.events = EventHandler()
+        self.connected = False
+        self.addr = (host, port)
+        self.protocol = protocol or self.protocol
+        if not self.protocol:
+            raise ValueError('Undefined protocol')
+
+        # Initialize raw socket
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect(self.addr)
+        self.connection = SocketConnection(self.socket, self.protocol)
+
+        # Set up event handlers
+        self.events.add_handler(self)
+        self.connection.recv_thread.events.bind('receive', lambda msg:
+            self.events.trigger('receive', msg))
+        self.connection.events.bind('close', lambda:
+            self.events.trigger('disconnect'))
+        self.events.bind('disconnect', self.disconnect)
+
+        # Connect!
+        self.connection.listen()
+        self.connected = True
+        self.events.trigger('connect')
+
+    def send(self, msg):
+        self.connection.send(msg)
+        self.events.trigger('send', msg)
+
+    def disconnect(self):
+        self.connection.close()
+        self.connected = False
+
+    def on_connect(self): pass
+    def on_send(self, msg): pass
+    def on_receive(self, msg): pass
+    def on_disconnect(self): pass
 
